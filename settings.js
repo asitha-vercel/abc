460123c17d4c0edab5dbc28ce96e83f93583cb30
asitha-vercel <asithavercel@gmail.com>
2026-07-22 08:00:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=FFsTFIYT#SRkVRjqS1lPNVKevMlBXwgZJfV55MPunIYFZxhmhlL8' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=sNNRgaZT#DfVyObXp63Mj2M62-nFisrtQcKKK33bCHU6rrEB5Cao' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
